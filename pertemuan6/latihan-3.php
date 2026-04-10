e507998f2fd68f5c54fb88b57376db382883cbeb
mzidanal22 <mzidanal99@gmail.com>
2026-04-10 14:17:14 +0700

    Update latihan pertemuan 6
    pengerjaan dan hasil pertemuan 6

---
+++ pertemuan6/latihan-3.php
@@ -0,0 +1,11 @@
+<?php
+$UsiaKaryawan["Lisa"] = "28";
+$UsiaKaryawan["Jack"] = "16";
+$UsiaKaryawan["Ryan"] = "35";
+$UsiaKaryawan["Rachel"] = "46";
+$UsiaKaryawan["Grace"] = "34";
+
+foreach($UsiaKaryawan as $nama => $umur){
+    echo "Nama Karyawan: " . $nama . ", Usia: " . $umur . " th <br>";
+}
+?>
