d4f9be8a2ebb33cafa7f22c888669ec26af0d138
mzidanal22 <mzidanal99@gmail.com>
2026-04-11 02:41:49 +0000

    pertemuan 4 update

---
+++ pertemuan 3/latihan3.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Penggunaan For</title>
+</head>
+<body>
+    <?php
+        for ($bil = 1; $bil < 25; $bil++) {
+            print ("$bil <br>\n");
+        }
+    ?> 
+</body>
+</html>
