721f178e5d04bf270075294f5b55793adf359c2d
mzidanal22 <mzidanal99@gmail.com>
2026-05-15 13:11:01 +0000

    update lat 3 pert 12

--- pertemuan12/latihan3.php
+++ pertemuan12/latihan3.php
@@ -5,14 +5,14 @@
     $dbname = "artikel_db";
     
     //lakukan koneksi dengan mysql
-    $connection = mysql_connect($dbhost,$dbuser,$dbpass);
+    $connection = mysqli_connect($dbhost,$dbuser,$dbpass);
     if(!$connection){
         echo "Tidak dapat terhubung dengan database";
         exit;
     }
     
     //pilih database 
-    $pilih_db = mysql_select_db($dbname);
+    $pilih_db = mysqli_select_db($connection, $dbname);
     if(!$pilih_db){
         echo "Tidak dapat memilih database";
         exit;
