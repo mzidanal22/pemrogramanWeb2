5a63a0c6bc04b090166ba44c18e6ff18e83a9a55
mzidanal22 <mzidanal99@gmail.com>
2026-05-15 13:18:44 +0000

    update lat 1 & 2 pert 12

--- pertemuan12/latihan1.php
+++ pertemuan12/latihan1.php
@@ -6,12 +6,18 @@ if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-mysqli_query($con, "
-    UPDATE tbl_mhs 
-    SET Age = '36'
-    WHERE FirstName = 'Karina' 
-    AND LastName = 'Swandi'
-");
+$query = "
+    UPDATE tbl_mhs
+    SET Age = '38'
+    WHERE FirstName = 'Kirana'
+    AND LastName = 'Swindari'
+";
+
+if (mysqli_query($con, $query)) {
+    echo "Data berhasil diubah";
+} else {
+    echo "Error: " . mysqli_error($con);
+}
 
 mysqli_close($con);
 
--- pertemuan12/latihan2.php
+++ pertemuan12/latihan2.php
@@ -1,12 +1,27 @@
 <?php
-    $con = mysqli_connect("localhost","root","");
-    if (!$con){
-        die('Could not connect: ' . mysqli_connect_error());
+
+$con = mysqli_connect("localhost", "root", "");
+
+if (!$con) {
+    die("Could not connect: " . mysqli_connect_error());
+}
+
+mysqli_select_db($con, "lat_dbase");
+
+$query = "DELETE FROM tbl_mhs WHERE LastName='Prabowo'";
+
+if (mysqli_query($con, $query)) {
+
+    if (mysqli_affected_rows($con) > 0) {
+        echo "Data berhasil dihapus";
+    } else {
+        echo "Data tidak ditemukan";
     }
 
-    mysqli_select_db($con, "lat_dbase");
+} else {
+    echo "Error: " . mysqli_error($con);
+}
 
-    mysqli_query($con, "DELETE FROM tbl_mhs WHERE LastName='Prabowo'");
+mysqli_close($con);
 
-    mysqli_close($con);
 ?>
