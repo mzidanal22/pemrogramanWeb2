2fdeddd775ba645c41b841cf7944fc339594dcaf
mzidanal22 <mzidanal99@gmail.com>
2026-05-27 15:18:45 +0000

    update pertemuan15

---
+++ pertemuan15/cookies3.php
@@ -0,0 +1,11 @@
+<?php
+
+// Menghapus cookie dengan mengatur waktu kadaluarsa ke masa lalu
+setcookie("username", "", time() - 3600);
+setcookie("namalengkap", "", time() - 3600);
+
+echo "<h1>Cookie berhasil dihapus.</h1>";
+echo "<h2>Klik <a href='cookie1.php'>di sini</a> untuk penciptaan cookies</h2>";
+echo "<h2>Klik <a href='cookie2.php'>di sini</a> untuk pemeriksaan cookies</h2>";
+
+?>
